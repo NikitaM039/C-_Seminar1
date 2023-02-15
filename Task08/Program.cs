138b31c6485c9dc14eac6d61bf4330a1fe0f91fe
Nikita Malyshev Aorus <nikita110097@mail.ru>
2023-02-15 23:55:53 +0200

    done all tasks

--- Task08/Program.cs
+++ Task08/Program.cs
@@ -2,6 +2,13 @@
 int N = Convert.ToInt32(Console.ReadLine());
 int temp = 1;
 while(temp<=N)
+if(temp % 2==0)
 {
     Console.Write($"{temp} ");
+    temp++;
+}
+
+else
+{
+    temp++;
 }
--- Task8.1/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿Console.Write("Введите число:");
-int N = Convert.ToInt32(Console.ReadLine());
-int temp = 1;
-while(temp<=N)
-{
-    Console.Write($"{temp} ");
-    temp++;
-}
