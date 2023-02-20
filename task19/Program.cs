02a93321cef202247651730ab603daac4d2ca523
Nikita Malyshev Aorus <nikita110097@mail.ru>
2023-02-20 21:58:48 +0200

    Done tasks 19,21,23

---
+++ task19/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число 
+// и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+string? Promt(string? message)
+{
+    Console.Write(message);
+    string? result = Convert.ToString(Console.ReadLine());
+    return result;
+}
+
+string? number = Promt ("Введите пятизначное число: ");
+// if (number<10000 || number>=100000)
+// {
+//     Console.WriteLine("Вы ввели не пятизначное число");
+//     return;
+// }
+
+Console.WriteLine($"Введенное число {number}");
+if (number[0] == number[4] && number[1] == number [3])
+{
+    Console.WriteLine("Это палендром");
+} 
+else
+{
+    Console.WriteLine("Это не палендром");
+}
