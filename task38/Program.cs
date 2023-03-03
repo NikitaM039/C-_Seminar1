4996e34fa2c08c12f73a32603202bd45a8f76ca0
Nikita Malyshev Aorus <nikita110097@mail.ru>
2023-03-03 10:15:10 +0200

    Done task 38

--- task38/Program.cs
+++ task38/Program.cs
@@ -4,37 +4,36 @@
 
 // [3 7 22 2 78] -> 76
 
-int[] FillArrayWithRandomNumbers(int length)
+double[] FillArrayWithRandomNumbers(int length)
 {
-    int[] arr = new int[length];
+    double[] arr = new double[length];
     Random rnd = new Random();
     for (int i = 0; i < arr.Length; i++)
     {
-        arr[i] = rnd.Next(-100, 100);
+        arr[i] = rnd.NextDouble() * 100;
     }
     return arr;
 }
 
 System.Console.WriteLine("Введите длину массива: ");
 int length = Convert.ToInt32(Console.ReadLine());
-int[] array = FillArrayWithRandomNumbers(length);
+double[] array = FillArrayWithRandomNumbers(length);
 System.Console.WriteLine($"[{string.Join(", ", array)}]");
-int minnumber = 0;
-int maxnumber = 0;
 
-    for (int i = 0; i < array.Length - 1; i++)
+double min = array[0];
+double max = array[0];
+
+for(int i = 0; i < array.Length; i++)
+{
+    if(max>array[i])
     {
-
-        for (int j = i+1; i < array.Length; j++)
-        {
-        if (array[j] < array[minnumber]) minnumber = j;
-        }
-        // for (int k = i + 1; i < array.Length; k++)
-        // {
-        //     if (array[k] < array[maxnumber]) maxnumber = k;
-        // }
-        
+        if(min<array[i])
+        {}
+        else
+        min=array[i];
     }
-
-Console.WriteLine($"Min = {minnumber}, Max = {maxnumber}");
-
+    else
+    max=array[i];
+}
+double result=max-min;
+System.Console.WriteLine($"Max = {max}, Min = {min}, result = {result}");
