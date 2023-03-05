f05259ffa434d9ac7071f7b714dc8f4ad2034335
Nikita Malyshev Aorus <nikita110097@mail.ru>
2023-03-05 14:09:18 +0200

    Done tasks 41, 43

---
+++ task41/Program.cs
@@ -0,0 +1,24 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+Console.Write("Введите длину массива = ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] a = new int[n];
+int count = 0;
+for (int i = 0; i < n; i++)
+{
+    {
+        Console.Write($"Число массива [{i + 1}] = ");
+        a[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    if (a[i] > 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"[{string.Join(", ", a)}]");
+Console.WriteLine($"{count}");
