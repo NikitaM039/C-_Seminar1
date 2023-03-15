73a099b39bab324884c1befca89cd38b8f86bb85
Nikita Malyshev Aorus <nikita110097@mail.ru>
2023-03-15 23:25:26 +0200

    Done tasks 64,66,68

---
+++ task64/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+void RoadTon(int N, int M = 1)
+{
+  if(N == M+1)
+  {
+    return;
+  }
+  RoadTon(N+1,M);
+  Console.Write($"{N} ");
+}
+
+System.Console.Write("Введите число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(1);
+RoadTon(number2, number1);
---
+++ task66/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+int CountNaturalSum(int m, int n)
+{
+    if (m == n)
+        return n;
+    return n + CountNaturalSum(m, n - 1);
+}
+
+System.Console.Write("Введите число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма элементов от {number1} до {number2} = {CountNaturalSum(number1, number2)}");
+
+
