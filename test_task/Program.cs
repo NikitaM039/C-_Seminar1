e7252fb175c0beedc4e533230ef6f77ed00f6bab
Nikita Malyshev Aorus <nikita110097@mail.ru>
2023-03-25 21:47:24 +0200

    Done last task

--- test_task/Program.cs
+++ test_task/Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿string[] array;
+Console.WriteLine("Введите массив строк через пробел");
+string? row = Console.ReadLine();
+array = row.Split(' ');
+string[] array2 = new string[array.Length];
+
+void SecondArrayWithIF(string[] array, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+    if(array[i].Length <= 3)
+        {
+        array2[count] = array[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array2[i]} ");
+    }
+}
+SecondArrayWithIF(array, array2);
+System.Console.WriteLine("Новый массив:");
+PrintArray(array2);
